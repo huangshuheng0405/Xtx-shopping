98d92dd1cf75718eae2493d4689775762a05a10d
Esther <3444104242@qq.com>
2025-12-06 17:10:04 +0800

    首页分类接口&数据类型

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,43 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
-//
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import CategoryPannel from '@/pages/index/components/CategoryPannel.vue'
+import { getHomeCategoryAPI } from '@/services/home'
+
+const bannerList = ref<BannerItem[]>([])
+//获取轮播图数据
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  // console.log(res)
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+  getHomeCategoryData()
+})
+// 获取前台数据分类
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  // console.log(res)
+  categoryList.value = res.result
+}
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
+  <CategoryPannel :list="categoryList"></CategoryPannel>
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
 //
+page {
+  background-color: #f7f7f7;
+}
 </style>
